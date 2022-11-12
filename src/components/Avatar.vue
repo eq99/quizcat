034f282361fbb814cd2f2ddeb8664773aca6f9dc
xiayulu <harolddfinchh@gmail.com>
2022-11-12 18:53:02 +0800

    feat: dropdown

---
+++ src/components/Avatar.vue
@@ -0,0 +1,48 @@
+<script  lang="ts" setup>
+// types
+
+// variables
+const props = defineProps({
+  link: {
+    type: String,
+    default: ''
+  },
+  text: {
+    type: String,
+    default: '登录'
+  }
+})
+</script>
+
+<template>
+  <div class="avatar">
+    <img v-if="link" :src="link" alt="">
+    <div v-else class="text">{{ text }}</div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.avatar {
+  width: 48px;
+  height: 48px;
+  cursor: pointer;
+
+  border-radius: 50%;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+  }
+
+  .text {
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,65 @@
+<script  lang="ts" setup>
+
+// types
+export type Position = "bottom-center" | 'bottom-right' | 'bottom-left';
+
+export interface Props {
+  position?: Position
+}
+
+//vars
+const props = withDefaults(defineProps<Props>(), {
+  position: 'bottom-center'
+});
+
+// states
+
+</script>
+
+<template>
+  <div class="drop">
+    <div class="hd-box">
+      <slot name='head'>
+      </slot>
+    </div>
+    <div class="bd-box" :class="position">
+      <slot name="body"></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.drop {
+  display: inline-block;
+  position: relative;
+
+  &:hover,
+  &.hover {
+    .bd-box {
+      visibility: visible;
+    }
+  }
+
+  .bd-box {
+    position: absolute;
+    visibility: hidden;
+    transition: .3s;
+
+    &.bottom-center {
+      top: 100%;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+
+    &.bottom-right {
+      top: 100%;
+      right: 0;
+    }
+
+    &.bottom-left {
+      top: 100%;
+      left: 0;
+    }
+  }
+}
+</style>
