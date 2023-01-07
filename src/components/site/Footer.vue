5e9d4fd0661f27de7f2652b6e1887e81bf3395d8
xiayulu <harolddfinchh@gmail.com>
2023-01-07 18:04:44 +0800

    add release

--- src/components/site/Footer.vue
+++ src/components/site/Footer.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="footer">
-    Copyright&copy;森喵喵 2021~2022
+    Copyright&copy;森喵喵 2021~2023
     <a href="https://beian.miit.gov.cn/" target="_blank" class="link">鄂ICP备2021018865号</a>
   </div>
 </template>
