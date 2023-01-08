41c521be13b8663ccbd947952cec2a86d141d84a
xiayulu <harolddfinchh@gmail.com>
2023-01-08 15:49:32 +0800

    refact: using auto-import

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,9 @@ import 'vditor/dist/index.css';
 
 import App from './App.vue'
 import router from './router'
-import naive from 'naive-ui'
 
 const app = createApp(App)
 
-app.use(naive)
 app.use(createPinia())
 app.use(router)
 
